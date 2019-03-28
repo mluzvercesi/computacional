e1e203cb4e312392dd5b1a3389576279305009a2
maria luz <mvercesi@ws22.labo2>
2019-03-28 15:56:25 -0300

    agregado clase 28/03

--- red.c
+++ red.c
@@ -10,14 +10,15 @@ int clasificar(int *red, int N);
 int etiquetafalsa(int *red, int *historial, int S1, int S2, int i);
 
 
-// MAIN
-int main(int argc, char * argv[])
+//------------MAIN-------------
+int main() //(int argc, char * argv[])
 	{
-	int N = atoi(argv[1]);
-	double P = atof(argv[2]);
+	int N = 8;
+	double P = 0.5;
+	//si ingreso argumentos a main uso las dos lineas que siguen
+	//int N = atoi(argv[1]); 
+	//double P = atof(argv[2]);
 	int *red;
-	printf("%s %s \n", "Lado", argv[1]);
-        printf("%s %s \n", "Prob", argv[2]);
 	red = (int*)malloc(N*N*sizeof(int));
 	
 	poblar(red,N,P);	
@@ -26,21 +27,22 @@ int main(int argc, char * argv[])
 	return 0;
 	}
 
+//----------FUNCIONES----------
 
-int poblar(int *red, int N, double P) //función que puebla la red
+int poblar(int *red, int N, double P) //llena la red cuadrada de lado N con prob P
 	{
 	int i;
 	float random;
 	srand(time(NULL));
 	for (i=0; i<N*N; i++)
-		{random = (float)rand()/(float)RAND_MAX;
+		{random = (float)rand()/(float)RAND_MAX; //se podria mejorar este random
 		if (random < P)
 			*(red+i) = 1;
 		}
 	return 0;
 	}
 
-int imprimir(int *red, int N)
+int imprimir(int *red, int N) //para mostrar la red creada
 	{
 	int i,j;
 	for (i=0; i<N; i++)
@@ -52,21 +54,60 @@ int imprimir(int *red, int N)
 	return 0;
 	}
 
-/*int clasificar(int *red, int N)
-	{
-	int i;
-	int frag; //etiqueta
-	frag = 2; //por que empezaba en 2??
-	for (i=0; i<N; i++) //recorro la primera fila
-	{if (*(red+i))
-		{*(red+i)=frag;
-		frag++;
-		}
-		
+// HASTA ACA FUNCIONA. A PARTIR DE ACA HAY QUE ESCRIBIR ACTUALIZAR
+
+int clasificar(int *red, int N)
+{
+	int i,j;
+	int frag = 2; //contador de fragmentos o clusters, por que empieza en 2??
+	
+	//el primer lugar de la red
+	if *(red)
+	{*(red)=frag;
+	frag++;
 	}
 	
-	return 0;
+	//la primera fila, solo miro el vecino a izq
+	for (i=1; i<N; i++)
+	{int S = *(red+i-1);
+	actualizar(red+i, historial, S, frag);
 	}
+	
+	//la primera columna, solo miro vecino arriba
+	for (i=1; i<N; i++)
+	{int S = *(red+N*(i-1));
+	actualizar(red+N*i, historial, S, frag);
+	
+		//el resto de la red, donde puede haber conflictos de etiquetas
+		for (j=1; j<N; j++)
+			{int S1 = *(red+N*(i-1)+j);
+			int S2 = *(red+N*i+j-1);
+			if (S1*S2) //hay conflicto porque ambos lugares tienen elementos
+			else //no hay conflicto porque solo un lugar tiene un elemento
+				{if S1 //el unico vecino es S1
+					actualizar(red+N*i+j, historial, S1, frag);
+				else //el unico vecino es S2
+					actualizar(red+N*i+j, historial, S2, frag);
+				}
+			}
+	}
+return 0;
+}
+
+/*
+actualizar:
+si S=0 incrementa frag y le pone etiqueta
+si S=/=0 copia la etiqueta VERDADERA (seguir el camino de etiquetas falsas)
+RECUERDO: para rastrear etiquetas verdaderas:
+while (*(historial+S)<0)
+	{S=-(*(historial+S));
+	}
+*local=S;
+
+OJO ACA! Si canchereo evaluando actualizar en (red+i)
+aca pongo un puntero local, porque ya le estoy diciendo 
+a que lugar de la memoria ir
+
 
 int etiquetafalsa(int *red, int *historial, int S1, int S2, int i)
 	{
